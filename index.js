f76cdc27c0fcdc91ddbf32b3dc6c9d25cb5be28e
Yue Su <yuesu@Mac-Pro.local>
2020-06-18 17:32:12 -0300

    task 2

--- index.js
+++ index.js
@@ -70,7 +70,25 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model
+    this.milesPerGallon = milesPerGallon
+    this.tank = 0
+    this.odometer = 0 
+  }
 
+  fill(gallons) { this.tank += gallons }
+  drive(distance) {
+    if (this.tank - distance / this.milesPerGallon > 0) {
+      this.odometer += distance
+      this.tank -= distance / this.milesPerGallon
+    } else {
+      this.odometer += this.tank * this.milesPerGallon
+      this.tank = 0
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
+  
 }
 
 /*
